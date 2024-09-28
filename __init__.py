d7bc1756f9d07ef0f8d45c7652c545825b5798b4
Vincenzo Ciancia <vincenzoml@gmail.com>
2024-09-28 21:05:54 +0000

    Added generic fetch routes

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import os
+import folder_paths
 
 from .anymatix_checkpoint_fetcher import AnymatixCheckpointFetcher, AnymatixCheckpointLoader
 
@@ -16,12 +17,37 @@ from aiohttp import web
 from server import PromptServer
 routes = PromptServer.instance.routes
 
-@routes.get("/output/{filename:.+}")
-async def serve_output(request):
-    file_path = os.path.abspath(f"output/{request.match_info['filename']}")    
-    if file_path.startswith(f"{os.path.abspath(os.getcwd())}/output") and os.path.isfile(file_path) and os.access(file_path, os.R_OK):
-        return web.FileResponse(file_path)
-    else:
-        return web.Response(text="File not found", status=404)
+
+    
+
+allowed_dirs=["output","input","models"] 
+
+@routes.get("/{basedir}/{filename:.+}")
+async def serve_file(request):
+    response = web.Response(text="File not found", status=404)
+    
+    basedir = request.match_info['basedir']
+    
+    if basedir in allowed_dirs:
+        file_path = os.path.abspath(f"{basedir}/{request.match_info['filename']}")    
+        if file_path.startswith(f"{os.path.abspath(os.getcwd())}/{basedir}") and os.path.isfile(file_path) and os.access(file_path, os.R_OK):
+            response = web.FileResponse(file_path)
+        
+    return response
+    
+resource_extensions=[".ckpt",".safetensors"]
+@routes.get("/resources")
+async def serve_resources(request):
+    print("resources?")
+    md = folder_paths.models_dir
+    def find(path):
+        return os.path.join(md,path)
+    dirs = [d for d in os.listdir(md) if os.path.isdir(find(d))]    
+    resources = {
+        dir: [f for f in os.listdir(find(dir)) if any(f.endswith(ext) for ext in resource_extensions)] for dir in dirs
+    }
+    return web.json_response(resources)
+    
+
     
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
