db3e9b44f0f3c5e805f7f0096c3530c9d9c10f49
Vincenzo Ciancia <vincenzoml@gmail.com>
2024-09-28 15:10:09 +0000

    added output file server

--- __init__.py
+++ __init__.py
@@ -12,4 +12,16 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "AnymatixCheckpointLoader": "Anymatix Checkpoint Loader"
 }
 
+from aiohttp import web
+from server import PromptServer
+routes = PromptServer.instance.routes
+
+@routes.get("/output/{filename:.+}")
+async def serve_output(request):
+    file_path = os.path.abspath(f"output/{request.match_info['filename']}")    
+    if file_path.startswith(f"{os.path.abspath(os.getcwd())}/output") and os.path.isfile(file_path) and os.access(file_path, os.R_OK):
+        return web.FileResponse(file_path)
+    else:
+        return web.Response(text="File not found", status=404)
+    
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
